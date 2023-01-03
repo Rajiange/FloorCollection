fe74b4a820f5d3e2d815326703a46c018a99fcd1
Rajiange <42678971+Rajiange@users.noreply.github.com>
2023-01-03 17:36:19 +0100

    [Ajout] Front pour la liste des collections a suivre pour le floor price

--- Back/server.js
+++ Back/server.js
@@ -16,7 +16,7 @@ app.get('/opensea/myCollection/:id', async (req, res) => {
     for (let i = 0; i < myCollection.length; i++) {
         const pokemon_data = await fetch(`https://api.opensea.io/api/v1/collection/` + myCollection[i]);
         const data = await pokemon_data.json();
-        collectionData.push({name: data.collection.name, floor_price: data.collection.stats.floor_price});
+        collectionData.push({name: data.collection.name, floor_price: data.collection.stats.floor_price, image: data.collection.image_url});
     }
     res.send(collectionData);
 })
